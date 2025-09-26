791b3bc5b8b2b3118bf4d2c9814a3576ad66fcad
D-i-V-ine <vdivyansh27@gmail.com>
2025-09-26 16:43:33 +0530

    modified code

--- SUM_of_two_numbers.c
+++ SUM_of_two_numbers.c
@@ -4,13 +4,12 @@ void main()
 {
     int a, b, c, i;
     printf("Enter first number : ");
-    scanf("%d",&a);
+    scanf("%d", &a);
     printf("Enter second number : ");
-    scanf("%d",&b);
+    scanf("%d", &b);
     printf("Enter third number : ");
-    scanf("%d",&c);
+    scanf("%d", &c);
 
-    printf("SUM of  %d %d %d = %d",a,b,c,(a+b+c));
+    printf("SUM of  %d %d %d = %d", a, b, c, (a + b + c));
     getch();
-    
 }
