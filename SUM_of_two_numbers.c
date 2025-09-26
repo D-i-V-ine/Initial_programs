b269b8c9786d1e011a59a4d82a938b52780b500d
D-i-V-ine <vdivyansh27@gmail.com>
2025-09-26 16:42:43 +0530

    modified coe

---
+++ SUM_of_two_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int a, b, c, i;
+    printf("Enter first number : ");
+    scanf("%d",&a);
+    printf("Enter second number : ");
+    scanf("%d",&b);
+    printf("Enter third number : ");
+    scanf("%d",&c);
+
+    printf("SUM of  %d %d %d = %d",a,b,c,(a+b+c));
+    getch();
+    
+}
