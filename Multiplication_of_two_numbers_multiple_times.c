da03d96a806bc0aa1d4028f71b2d7abbaa49cac7
Divyansh Verma <vdivyansh27@gmail.com>
2025-09-27 13:50:07 +0530

    new code

---
+++ Multiplication_of_two_numbers_multiple_times.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int a, b, c;
+    char i;
+    for (i = 'y'; i == 'y' || i == 'Y';)
+    {
+
+        printf("Enter first number : ");
+        scanf("%d", &a);
+        printf("Enter second number : ");
+        scanf("%d", &b);
+        printf("Enter third number : ");
+        scanf("%d", &c);
+
+        printf("%d x %d x %d = %d", a, b, c, (a * b * c));
+
+        printf("\nDo you want to continue (y/n) : ");
+        i = getche();
+        printf("\n");
+    }
+}
