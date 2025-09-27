12a4464c1d4317713707c0ca0bd160a64a232b7b
Divyansh Verma <vdivyansh27@gmail.com>
2025-09-27 13:17:43 +0530

    new code

---
+++ Sum_of_multiple_numbers .c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int a, b, c, ;
+    char i;
+    for (i = 'y'; i == 'y' && i == 'Y';)
+    {
+
+        printf("Enter first number : ");
+        scanf("%d", &a);
+        printf("Enter second number : ");
+        scanf("%d", &b);
+        printf("Enter third number : ");
+        scanf("%d", &c);
+
+        printf("%d + %d + %d = %d", a, b, c, (a + b + c));
+        printf("\nDo you want to continue (y/n) : ");
+        scanf(" %c", &i);
+    }
+}
