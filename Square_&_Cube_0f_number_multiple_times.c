089800d46086fb40b840910c67230bf835311f5a
Divyansh Verma <vdivyansh27@gmail.com>
2025-09-30 16:08:24 +0530

    NEW code

---
+++ Square_&_Cube_0f_number_multiple_times.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int num, sq, cu;
+    char a = 'y';
+
+    for (a; a == 'y' || a == 'Y';)
+    {
+        printf("Enter a number : ");
+        scanf("%d", &num);
+        sq = num * num;
+        cu = sq * num;
+        printf("Square = %d\nCube = %d", sq, cu);
+        printf("\nDo you want to continue (y/n) : ");
+        a = getche();
+        printf("\n");
+    }
+}
