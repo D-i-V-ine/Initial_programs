6c6c14d2e41b40b9ff946efacea3181d5e157931
Divyansh Verma <vdivyansh27@gmail.com>
2025-10-10 21:33:02 +0530

    new code

---
+++ Factorial.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int x, y = 1;
+    printf("Enter your number : ");
+    scanf("%d", &x);
+    if (x < 0)
+    {
+        printf("Factorial of a negative number doesn't exist");
+    }
+    else if(x>0)
+    {
+        if (x != 0)
+        {
+            for (x; x > 0; x--)
+            {
+                y = y * x;
+            }
+        }
+        printf("Factorial = %d", y);
+    }
+  
+    else
+    {
+        printf("Enter valid input");
+    }
+}   
