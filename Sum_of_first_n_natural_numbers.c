aefbff2f5f7b2c964ee915df7512db6606bc3e07
Divyansh Verma <vdivyansh27@gmail.com>
2025-10-10 21:02:44 +0530

    new code

---
+++ Sum_of_first_n_natural_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int n, sum = 0, i;
+    printf("Enter any natural number: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; ++i)
+    {
+        sum += i;
+    }
+    printf("Sum of first %d natural numbers = %d", n, sum);
+}
