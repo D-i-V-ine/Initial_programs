ba8ad26e28efa4d36de4c6685949d95d6153c6d2
Divyansh Verma <vdivyansh27@gmail.com>
2025-10-10 21:19:53 +0530

    new code

---
+++ Sum_of_all_digits.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int a;
+    int sum = 0;
+    printf("Enter your  number : ");
+    scanf("%d", &a);
+    for (a; a > 0; a=(a / 10))
+    {
+        sum += (a % 10);
+    }
+    printf("Sum of all digits = %d", sum);
+}
