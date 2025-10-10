b203521f65414c641e09d5c47465d1bec4770ef0
Divyansh Verma <vdivyansh27@gmail.com>
2025-10-10 21:19:40 +0530

    new code

---
+++ Number_of_digits.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+
+{
+    int i = 0, a;
+
+    printf("Enter your number : ");
+    scanf("%d", &a);
+    for (a; a > 0; a = (a / 10))
+    {
+        i++;
+    }
+    printf("Number of digits = %d", i);
+}
