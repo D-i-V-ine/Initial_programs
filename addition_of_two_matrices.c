e5eeb8a2af4f861f02a3e63d37813743e5369a30
Divyansh Verma <vdivyansh27@gmail.com>
2025-10-14 18:34:48 +0530

    new code

---
+++ addition_of_two_matrices.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int A[10][10], B[10][10];
+    int i, j, k, r, c;
+    printf("Enter of rows (<10) : ");
+    scanf("%d", &r);
+    printf("Enter of columns (<10) : ");
+    scanf("%d", &c);
+
+    for (k = 0; k < 2; k++)
+    {
+        for (i = 0; i < r; i++)
+        {
+            for (j = 0; j < c; j++)
+            {
+                if (k == 0)
+                {
+                    printf("Enter A(%d,%d) : ", i + 1, j + 1);
+                    scanf("%d", &A[i][j]);
+                }
+                else
+                {
+                    printf("Enter B(%d,%d) : ", i + 1, j + 1);
+                    scanf("%d", &B[i][j]);
+                }
+            }
+        }
+        printf("\n");
+    }
+    printf("\nMatrix A : \n");
+    for (i = 0; i < r; i++)
+    {
+        for (j = 0; j < c; j++)
+        {
+            printf("%d ", A[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nMatrix B : \n");
+    for (i = 0; i < r; i++)
+    {
+        for (j = 0; j < c; j++)
+        {
+            printf("%d ", B[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nSum of Matrix A and B : \n");
+    for (i = 0; i < r; i++)
+    {
+        for (j = 0; j < c; j++)
+        {
+            printf("%d ", A[i][j] + B[i][j]);
+        }
+        printf("\n");
+    }
+}
