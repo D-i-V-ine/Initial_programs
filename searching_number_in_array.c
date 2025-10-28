4375e72fbfbbd49ee65f09e390fd9ab3021549ba
Divyansh Verma <vdivyansh27@gmail.com>
2025-10-28 17:21:07 +0530

    add multiple times search feature

--- searching_number_in_array.c
+++ searching_number_in_array.c
@@ -3,7 +3,7 @@
 void main()
 {
     int A[100];
-    int a, b, c, i;
+    int a, b, c, i, j;
     printf("\nPress any alphatic key to STOP\n");
     i = 0;
     do
@@ -19,19 +19,24 @@ void main()
     while (getchar() != '\n')
         ;
 
-    printf("\nEnter number to be searched: ");
-    scanf(" %d", &c);
-
-    while (i--)
+    j = i;
+    do
     {
-        if (A[i] == c)
+        printf("\nEnter number to be searched: ");
+        scanf(" %d", &c);
+        i = j;
+        while (i--)
         {
-            printf("%d found at position %d", c, i + 1);
-            break;
+            if (A[i] == c)
+            {
+                printf("%d found at position %d\n", c, i + 1);
+                break;
+            }
+            if (i == 0)
+            {
+                printf("Number not found\n");
+            }
         }
-    }
-    if (i == -1)
-    {
-        printf("Number not found\n");
-    }
+        printf("\nSearch another number? (Y/N): ");
+    } while (getche() == 'y' || getche() == 'Y');
 }
