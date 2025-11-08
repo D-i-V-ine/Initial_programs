15e6cec05ca185319e75a90156b5ca9b00c6e14e
Divyansh Verma <vdivyansh27@gmail.com>
2025-11-08 21:47:09 +0530

    new code

---
+++ dynamic_array.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main()
+{
+    int i, j, a, n;
+    int *p;
+    printf("Enter any alphabet key for exiting\n");
+    p = malloc(1 * sizeof(int));
+    i = 0;
+    while (1)
+    {
+        printf("Enter the number of element: ");
+        a = scanf("%d", &n);
+        *(p + i) = n;
+        if (a == 1)
+        {
+            i++;
+            p = realloc(p, (i + 1) * sizeof(int));
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    while (getchar() != '\n')
+        ;
+
+    int A[i];
+    for (j = 0; j < i; j++)
+    {
+        A[j] = *(p + j);
+        printf("%d ", A[j]);
+        printf("\n");
+    }
+    free(p);
+}
