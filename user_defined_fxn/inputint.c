01753f778f7a8c6456cbe78848f9541aa6f3a18e
Divyansh Verma <vdivyansh27@gmail.com>
2025-11-08 23:27:08 +0530

    new codes

---
+++ user_defined_fxn/inputint.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+int j;
+int *inputint(int *c)
+{
+    int *p;
+    int i, j, a, n;
+    printf("Enter any alphabet key for exiting\n");
+    p = malloc(1 * sizeof(int));
+    i = 0;
+    while (1)
+    {
+        printf("Enter the number of element: ");
+        a = scanf("%d", &n);
+        *(p + i) = n;
+        if (a == 1)
+        {
+            i++;
+            p = realloc(p, (i + 1) * sizeof(int));
+        }
+        else
+        {
+            while (getchar() != '\n')
+                ;
+            break;
+        }
+    }
+    *c = i;
+    return p;
+}
+//gives the number of elements through pointer c
+//and returns the pointer p
---
+++ user_defined_fxn/user_entered_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int i, a;
+    int A[100];
+    printf("Enter \"any alphabet\" to stop entering elements in array\n");
+    i = 0;
+    while (1)
+    {
+        printf("Enter input : ");
+        a = scanf("%d", &A[i]);
+        if (a == 0)
+        {
+            break;
+        }
+        i++;
+    }
+    printf("Array : ");
+    printf("{");
+    for (int j = 0; j < i; j++)
+    {
+        printf("%d", A[j]);
+        if (j != i - 1)
+        {
+            printf(",");
+        }
+    }
+    printf("}");
+}
